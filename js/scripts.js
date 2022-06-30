41d582cf56f37ea4c3a793c2259c40010cad962d
José Alisson <josealissonbr2003@outlook.com>
2022-06-30 10:43:38 -0300

    add jump

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
-document.addEventListener("keypress", function(event) {
-    var mario = document.getElementById('mario');
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const marioJump = (event) => {
+
     if (event.code == "Space"){
         mario.classList.add("mario-jump");
         setTimeout(function () {
             mario.classList.remove("mario-jump");
-        }, 400)
+        }, 400);
     }
-});
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    
+    if (pipePosition <= 100){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
+    }
+
+}, 10);
+
+document.addEventListener("keypress", marioJump);
